5f74b65255b015f79690cc80fe135bd8747306b1
Keyner Lizarazo <keynerlizarazo655@gmail.com>
2025-04-20 21:25:11 -0400

    Primeros ajsutes de backend y crud
    -falta hacer que funcione el apartado de editar

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cedulas, name='cedulas'),  # Ruta principal para listar cédulas
-    path('eliminar/<int:id>/', views.eliminar_cedula, name='eliminar_cedula'),  # Ruta para eliminar
+    path('', views.login_view, name='login'),  # Ruta para el login
+    path('logout/', views.logout_view, name='logout'),  # Ruta para cerrar sesión
+    path('cedulas/', views.cedulas, name='cedulas'),  # Ruta protegida para cedulas.html
+    path('eliminar/<int:id>/', views.eliminar_cedula, name='eliminar_cedula'),  # Ruta para eliminar un registro
+    path('guardar/', views.guardar_cedula, name='guardar_cedula'),  # Nueva ruta para guardar/actualizar registros
 ]
